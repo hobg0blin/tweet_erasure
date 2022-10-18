f610ba1b98736cc4b948e0df5a573fa1a4c8e406
Brent <bb2906@nyu.edu>
2022-10-17 21:23:49 -0400

    c o m m e n t s

--- erasure.py
+++ erasure.py
@@ -9,40 +9,41 @@ with open("corpuses/rondesantisfl_tweets.csv", newline='') as csvfile:
     fascismreader = list(reader)
 
     outputrows = []
+    #get 900 lines from baraka
     for line in baraka[10:1000]:
         clean_line = line.strip()
         if (len(clean_line) > 1) :
             good_index = 0
             output_phrase = []
+            # for each line, check each tweet in order for matches
             for row in fascismreader:
+                # get words from Bad Text
                 bad_words = row['text'].split()
-                # get characters from bad text and try to match next character in good text
                 for word in bad_words:
-                    #fucked this up by trying to split for a very long time, was only gettuing full words, guess you can't split a word with no delimiter?
+                # get characters from Bad Words and try to match next character in good text
+                    #fucked this up by trying to split for a very long time, was only getting full words, guess you can't split a word with no delimiter?
                     # letters = word.split()
                     letters = list(word)
                     for letter in letters:
-#                            print('letter: ', letter)
-#                            print('clean letter: ', clean_line[good_index])
                         if (good_index < len(clean_line)):
+                        #compare current letter in line from baraka text with letters in tweet, if it hits, add to output, otherwise add a space
                             if letter == clean_line[good_index]:
                                 output_phrase.append(clean_line[good_index])
                                 good_index += 1
                             elif (good_index >= 1):
                                 output_phrase.append(" ")
+                # once our output_phrase "tweet" hits 280 chars, add it to our output array and then reset it
                 if (len(output_phrase) >= 280):
-                    print('phrase len: ', len("".join(output_phrase).strip()))
+                    #make sure it's not totally blank
                     if (len("".join(output_phrase).strip()) > 0) :
                         outputrows.append("".join(output_phrase))
 
                     output_phrase = []
-#                    print('letter found! ' "".join(output_phrase))
 output = {'tweets': outputrows}
 print('output: ', output)
 json_output = json.dumps(output, indent=4)
 with open("output.json", "w") as outfile:
     outfile.write(json_output)
-#
 
 
 
